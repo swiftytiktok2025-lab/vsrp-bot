58190019d59ea22878da40099edee45ff3295930
Aidan <swiftytiktok2025@gmail.com>
2026-04-14 22:12:18 -0500

    Added express for uptime

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🌐 Web server running on port ${PORT}`);
+});
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 // 💎 PREMIUM DONATION EMBED
 const donationEmbed = new EmbedBuilder()
